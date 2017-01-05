966c8ee4290ee2104e1aa1df72459949a2df177e
jindong <281027818@qq.com>
2017-01-05 10:55:00 +0800

    谷歌地图

---
+++ googleMap.js
@@ -0,0 +1,23 @@
+﻿var map;
+function initMap(x, y, tit, cont) {
+    var mapCenter = { lat: parseFloat(x), lng: parseFloat(y) };
+    //========创建基本地图========
+    map = new google.maps.Map(document.getElementById('mapContainer'), {
+        center: mapCenter,
+        zoom: 15
+    });
+    //========创建地图标记========
+    var marker = new google.maps.Marker({
+        position: mapCenter,
+        map: map,   //指定放在哪个地图中，也可以使用marker.setMap(map);移除：marker.setMap(null)
+        title: 'Hello World!',
+        icon: '[iconpath]'
+    });
+    //========创建地图信息窗========
+    var contentString = '<div id="content"><h3>' + tit + '</h3><p>' + cont + '</p></div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    infowindow.open(map, marker);
+
+}
